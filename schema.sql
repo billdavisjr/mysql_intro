baa859d781d6ec28ad456200a70a2477d04bf8fb
Bill Davis <bill.davis@gmail.com>
2020-05-09 04:49:54 +0000

    Added SQL script to generate schema.txt file

---
+++ schema.sql
@@ -0,0 +1,45 @@
+system rm schema.txt
+tee schema.txt;
+
+Show Databases;
+
+Use Chinook;
+Show Tables;
+
+\! echo Album 
+Describe Album;
+
+system echo Artist;
+Describe Artist;
+
+system echo Customer;
+Describe Customer;
+
+system echo Employee;
+Describe Employee;
+
+system echo Friends;
+Describe Friends;
+
+system echo Genre;
+Describe Genre;
+
+system echo Invoice;
+Describe Invoice;
+
+system echo InvoiceLink;
+Describe InvoiceLine;
+
+system echo MediaType;
+Describe MediaType;
+
+system echo PlayList;
+Describe Playlist;
+
+system echo PlayListTrack;
+Describe PlaylistTrack;
+
+system echo Track;
+Describe Track;
+
+notee;
